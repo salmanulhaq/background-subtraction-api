c77d28c70e710ddddca9c2b8689aebd35c976a81
Salman <salman@programmerfish.com>
2016-09-16 17:32:47 +0300

    first commit, working api

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 echo "Starting installation of image background subtraction module"
 sudo apt-get update
 sudo apt-get install -y build-essential libopencv-dev libcurl4-gnutls-dev nodejs npm
+sudo ln -sf /usr/bin/nodejs /usr/bin/node
 cd node-bgsub && sudo cp bgsub.pc /usr/lib
 npm install
 cd -
