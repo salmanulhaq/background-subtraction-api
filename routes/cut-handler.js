e05dd17e2f3de11a38985b932fe9a154e46071f2
Salman <salman@programmerfish.com>
2016-09-16 17:57:19 +0300

    updated

--- routes/cut-handler.js
+++ routes/cut-handler.js
@@ -10,16 +10,18 @@ router.get('/', function(request, response, next) {
 	
 	if (!params.url || params.url == '') {
 		response.status(501).json({status:'ERROR', message: 'You must specify "url" param'});
+		return next();
 	}
 	
 	bgsub.subtract(params.url, function(error, contentLength, outputImage){
 		if(error) {
 			response.status(501).json({status:'ERROR', message: error});
+			return next();
 		} else {
 			response.header('Content-Type', 'image/png');
             response.header('Content-Length', contentLength);
 			response.status(200).send(outputImage);
-            next();
+            return next();
 		}
 	});
 });
